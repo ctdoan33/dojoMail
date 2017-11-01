07f3559058126a9c255e6afcad983deaf5c16265
ctdoan33 <doanchristopher_1@yahoo.com>
2017-10-31 18:42:31 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  headers = ['email', 'importance', 'subject', 'content'];
+  emails = [
+	  {'email':'bill@gates.com', 'importance':true, 'subject':'New Windows', 'content': 'Windows XI will launch in year 2100.'},
+	  {'email':'ada@lovelace.com', 'importance':true, 'subject':'Programming', 'content': 'Enchantress of Numbers'},
+	  {'email':'john@carmac.com', 'importance':false, 'subject':'Updated Algo', 'content': 'New algorithm for shadow volumes.'},
+	  {'email':'gabe@newel.com', 'importance':false, 'subject':'HL3!', 'content': 'Just kidding...'}
+  ];
 }
